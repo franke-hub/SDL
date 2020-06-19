cdde1414fc374e77ebfe03c4a89b50035db79b40
Frank Eskesen <f.n.eskesen@gmail.com>
2020-06-18 21:50:27 -0400

    Allocator object created.

---
+++ src/cpp/inc/pub/Allocator.h
@@ -0,0 +1,101 @@
+//----------------------------------------------------------------------------
+//
+//       Copyright (c) 2020 Frank Eskesen.
+//
+//       This file is free content, distributed under the Lesser GNU
+//       General Public License, version 3.0.
+//       (See accompanying file LICENSE.LGPL-3.0 or the original
+//       contained within https://www.gnu.org/licenses/lgpl-3.0.en.html)
+//
+//----------------------------------------------------------------------------
+//
+// Title-
+//       Allocator.h
+//
+// Purpose-
+//       Storage allocator description.                                                                 ts.
+//
+// Last change date-
+//       2020/06/18
+//
+//----------------------------------------------------------------------------
+#ifndef _PUB_ALLOCATOR_H_INCLUDED
+#define _PUB_ALLOCATOR_H_INCLUDED
+
+#include <sys/types.h>              // For size_t
+#include "config.h"                 // For _PUB_NAMESPACE
+
+namespace _PUB_NAMESPACE {
+//----------------------------------------------------------------------------
+//
+// Class-
+//       Allocator
+//
+// Purpose-
+//       Allocator descriptor. (Base class)
+//
+// Implementation notes-
+//       The generic Allocator allocates variable size blocks. However,
+//       Allocator subclasses may restrict usage to one fixed length
+//       block size. These implementations MAY choose to ignore the
+//       (defaulted) get and put length option.
+//
+//       The base class is implmented using malloc/free. This put method
+//       implementation ignores the length parameter.
+//
+//----------------------------------------------------------------------------
+class Allocator {                   // Allocator descriptor
+//----------------------------------------------------------------------------
+// Allocator::Attributes
+//----------------------------------------------------------------------------
+// The base class does not define attributes.
+
+//----------------------------------------------------------------------------
+// Allocator::Destructor/Constructor/Assignment
+//----------------------------------------------------------------------------
+public:
+virtual
+   ~Allocator( void ) = default;    // Default Destructor
+   Allocator( void ) = default;     // Default Constructor
+
+   Allocator(const Allocator&) = delete; // NO copy constructor
+Allocator&
+   operator=(const Allocator&) = delete; // NO assignment operator
+
+//----------------------------------------------------------------------------
+//
+// Method-
+//       get
+//
+// Purpose-
+//       Allocate storage
+//
+// Implementation notes-
+//       DOES NOT return nullptr, but may throw std::bad_alloc.
+//       A fixed length (block) Allocator MAY ignore the length option.
+//
+//----------------------------------------------------------------------------
+public:
+virtual void*                       // The allocated storage (never nullptr)
+   get(                             // Allocate storage
+     size_t            size= 0);    // Of this length
+
+//----------------------------------------------------------------------------
+//
+// Method-
+//       put
+//
+// Purpose-
+//       Release storage.
+//
+// Implementation notes-
+//       The size option is ignored for fixed length (block) allocators.
+//
+//----------------------------------------------------------------------------
+virtual void
+   put(                             // Deallocate
+     void*             addr,        // This storage
+     size_t            size= 0);    // Of this length (Optional)
+}; // class Allocator
+}  // namespace _PUB_NAMESPACE
+#endif // _PUB_ALLOCATOR_H_INCLUDED
---
+++ src/cpp/lib/pub/Allocator.cpp
@@ -0,0 +1,66 @@
+//----------------------------------------------------------------------------
+//
+//       Copyright (c) 2020 Frank Eskesen.
+//
+//       This file is free content, distributed under the Lesser GNU
+//       General Public License, version 3.0.
+//       (See accompanying file LICENSE.LGPL-3.0 or the original
+//       contained within https://www.gnu.org/licenses/lgpl-3.0.en.html)
+//
+//----------------------------------------------------------------------------
+//
+// Title-
+//       Allocator.cpp
+//
+// Purpose-
+//       Allocator method implementations.
+//
+// Last change date-
+//       2020/06/18
+//
+//----------------------------------------------------------------------------
+#include <exception>                // For std::bad_alloc
+#include <stdlib.h>                 // For malloc, free
+
+#include "pub/Allocator.h"
+
+namespace _PUB_NAMESPACE {
+//----------------------------------------------------------------------------
+//
+// Method-
+//       Allocator::get
+//
+// Purpose-
+//       Allocate storage
+//
+// Implementation notes-
+//       DOES NOT return nullptr, but may throw std::bad_alloc.
+//       A fixed length (block) Allocator MAY ignore the length option.
+//
+//----------------------------------------------------------------------------
+void*                               // The allocated storage (never nullptr)
+   Allocator::get(                  // Allocate storage
+     size_t            size)        // Of this length
+{
+   void* result= malloc(size);      // Allocate storage
+   if( result == nullptr )          // Exception if failure
+     throw std::bad_alloc();
+
+   return result;
+}
+
+//----------------------------------------------------------------------------
+//
+// Method-
+//       Allocator::put
+//
+// Purpose-
+//       Release storage.
+//
+//----------------------------------------------------------------------------
+void
+   Allocator::put(                  // Deallocate
+     void*             addr,        // This storage
+     size_t            size)        // Of this length
+{  free(addr); }                    // Release storage, ignoring size
+}  // namespace _PUB_NAMESPACE
