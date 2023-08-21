4902d25a25178bd883375bbcdb2e06b00a8a7a45
Frank Eskesen <f.n.eskesen@gmail.com>
2023-08-21 15:34:24 -0400

    .gitignore corrections

---
+++ src/py/lib/Test/_Make.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+##############################################################################
+##
+##       Copyright (C) 2023 Frank Eskesen.
+##
+##       This file is free content, distributed under the GNU General
+##       Public License, version 3.0.
+##       (See accompanying file LICENSE.GPL-3.0 or the original
+##       contained within https://www.gnu.org/licenses/gpl-3.0.en.html)
+##
+##############################################################################
+##
+## Title-
+##       _Make.py
+##
+## Purpose-
+##       Compile all programs in subdirectory.
+##
+## Last change date-
+##       2023/08/21
+##
+##############################################################################
+
+import Main
