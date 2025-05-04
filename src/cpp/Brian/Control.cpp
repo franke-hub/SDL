99bc9d661a823fd08afa50cce841d3dcd24e8c66
Frank Eskesen <f.n.eskesen@gmail.com>
2025-05-03 21:45:34 -0400

    Corrected copyright date.

--- src/cpp/Brian/Control.cpp
+++ src/cpp/Brian/Control.cpp
@@ -1,6 +1,6 @@
 //----------------------------------------------------------------------------
 //
-//       Copyright (c) 2019-2024 Frank Eskesen.
+//       Copyright (c) 2019-2025 Frank Eskesen.
 //
 //       This file is free content, distributed under the GNU General
 //       Public License, version 3.0.
