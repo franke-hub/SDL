294abc66b92b241596f27f86b0c7a33f88ed1e8f
Frank Eskesen <f.n.eskesen@gmail.com>
2026-03-17 11:47:09 -0400

    Added public domain copyright.

--- src/cpp/Test/Test_mutex.cpp
+++ src/cpp/Test/Test_mutex.cpp
@@ -1,5 +1,15 @@
 //----------------------------------------------------------------------------
 //
+//       Copyright (C) 2026 Frank Eskesen.
+//
+//       This file is free content, distributed under creative commons CC0,
+//       explicitly released into the Public Domain.
+//       (See accompanying html file LICENSE.ZERO or the original contained
+//       within https://creativecommons.org/publicdomain/zero/1.0/legalcode)
+//
+// SPDX-License-Identifier: CC0-1.0
+//----------------------------------------------------------------------------
+//
 // Title-
 //       Test_mutex.cpp
 //
